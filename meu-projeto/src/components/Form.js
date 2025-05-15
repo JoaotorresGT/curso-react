f1b0bd637a0da85413495dfa2889f4ba7621dd3e
João Torres <joaopedro.torres177@gmail.com>
2025-05-15 13:48:41 -0300

    aula 10
    Aprendendo a usar useState na pratica

--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,16 +1,27 @@
+import { useState } from "react"
+
 function Form () {
 
     function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log(`Cadastrou o usuario`)
+        console.log(name)
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return(
         <div>
             <h1>Meu cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"></input>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" id="name" name="name" placeholder="Digite o seu nome" onChange={(e) => setName(e.target.value)}></input>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" id="password" name="password" placeholder="Digite a sua senha" onChange={(e) => setPassword(e.target.value)} ></input>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"></input>
